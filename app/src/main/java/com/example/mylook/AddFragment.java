49fb9320995671b32611b4e823514984ef4a7fdf
Eugene Grigorian <grigorjan.bigmen@gmail.com>
2021-12-07 12:52:11 +0300

    simple camera interface

--- app/src/main/java/com/example/mylook/AddFragment.java
+++ app/src/main/java/com/example/mylook/AddFragment.java
@@ -1,20 +1,107 @@
 package com.example.mylook;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
 public class AddFragment extends Fragment {
+
+    private ImageView preview;
+    private static final int PERMISSION_REQUEST_CAMERA = 83821;
+    ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    YUVtoRGB translator = new YUVtoRGB();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        preview = view.findViewById(R.id.preview);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA},
+                    PERMISSION_REQUEST_CAMERA);
+        } else {
+            initializeCamera();
+        }
+        return view;
     }
+
+
+
+    private void initializeCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(getActivity());
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+//                    Preview preview = new Preview.Builder().build();
+//                    ImageCapture imageCapture = new ImageCapture.Builder().build();
+
+                    ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                            .setTargetResolution(new Size(1024, 768))
+                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                            .build();
+
+                    CameraSelector cameraSelector = new CameraSelector.Builder()
+                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                            .build();
+
+                    imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(getActivity()),
+                            new ImageAnalysis.Analyzer() {
+                                @Override
+                                public void analyze(@NonNull ImageProxy image) {
+                                    Image img = image.getImage();
+                                    Bitmap bitmap = translator.translateYUV(img, getActivity());
+                                    preview.setRotation(image.getImageInfo().getRotationDegrees());
+                                    preview.setImageBitmap(bitmap);
+                                    image.close();
+                                }
+                            });
+
+                    cameraProvider.bindToLifecycle(getActivity(), cameraSelector, imageAnalysis);
+
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(getActivity()));
+    }
+
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        preview = getView().findViewById(R.id.preview);
+//    }
 }
