c33fe8c0568ef7fd49e086b9a3009c5dfdb81dd2
Gahare <prohorshubenkov12@gmail.com>
2021-12-13 21:19:14 +0300

    added icons to main added buttons and image lists to closet

--- app/src/main/java/com/example/mylook/ClosetFragment.java
+++ app/src/main/java/com/example/mylook/ClosetFragment.java
@@ -5,16 +5,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class ClosetFragment extends Fragment {
-
-    @Nullable
+public class ClosetFragment extends Fragment{
+    Fragment childFragment;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_closet, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        insertNestedFragment();
+    }
+
+    public void insertNestedFragment() {
+        Fragment childFragment = new ShkafButtons();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.child_fragment_container, childFragment).commit();
+    }
+    public void swapnizlist() {
+        Fragment childFragment = new NizList();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.child_fragment_container, childFragment).commit();
+    }
+
+    public static class ShkafButtons extends Fragment{
+        ImageView nizButton;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_shkaf_buttons, container, false);
+            nizButton = (ImageView) view.findViewById(R.id.niz);
+            nizButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ClosetFragment parentfrag=((ClosetFragment)ShkafButtons.this.getParentFragment());
+                    parentfrag.swapnizlist();
+                    /*FragmentManager manager = getChildFragmentManager();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    transaction.replace(R.id.child_fragment_container, new NizList());
+                    transaction.commit();*/
+                }
+            });
+            return view;
+        }
+    }
+
+    public static class NizList extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_niz_list, container, false);
+        }
     }
 }
