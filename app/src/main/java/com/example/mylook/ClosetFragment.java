2498f9f181c2d24deead1dbcabc1b458d6b70d57
Gahare <prohorshubenkov12@gmail.com>
2021-12-16 17:20:07 +0300

    activated more buttons added more pictures

--- app/src/main/java/com/example/mylook/ClosetFragment.java
+++ app/src/main/java/com/example/mylook/ClosetFragment.java
@@ -37,9 +37,29 @@ public class ClosetFragment extends Fragment{
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.replace(R.id.child_fragment_container, childFragment).commit();
     }
+    public void swapverhlist() {
+        Fragment childFragment = new VerhList();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.child_fragment_container, childFragment).commit();
+    }
+    public void swapverhnyalist() {
+        Fragment childFragment = new VerhnyaList();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.child_fragment_container, childFragment).commit();
+    }
+    public void swapobuvlist() {
+        Fragment childFragment = new ObuvList();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.child_fragment_container, childFragment).commit();
+    }
+    public void swapslitnoelist() {
+        Fragment childFragment = new SlitnoeList();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.child_fragment_container, childFragment).commit();
+    }
 
     public static class ShkafButtons extends Fragment{
-        ImageView nizButton;
+        ImageView nizButton,obuvButton,verhButton,verhnyaButton,slitnoeButton;
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
@@ -50,10 +70,38 @@ public class ClosetFragment extends Fragment{
                 public void onClick(View v) {
                     ClosetFragment parentfrag=((ClosetFragment)ShkafButtons.this.getParentFragment());
                     parentfrag.swapnizlist();
-                    /*FragmentManager manager = getChildFragmentManager();
-                    FragmentTransaction transaction = manager.beginTransaction();
-                    transaction.replace(R.id.child_fragment_container, new NizList());
-                    transaction.commit();*/
+                }
+            });
+            slitnoeButton = (ImageView) view.findViewById(R.id.slitnoe);
+            slitnoeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ClosetFragment parentfrag=((ClosetFragment)ShkafButtons.this.getParentFragment());
+                    parentfrag.swapslitnoelist();
+                }
+            });
+            obuvButton = (ImageView) view.findViewById(R.id.obuv);
+            obuvButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ClosetFragment parentfrag=((ClosetFragment)ShkafButtons.this.getParentFragment());
+                    parentfrag.swapobuvlist();
+                }
+            });
+            verhButton = (ImageView) view.findViewById(R.id.verh);
+            verhButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ClosetFragment parentfrag=((ClosetFragment)ShkafButtons.this.getParentFragment());
+                    parentfrag.swapverhlist();
+                }
+            });
+            verhnyaButton = (ImageView) view.findViewById(R.id.verhnya);
+            verhnyaButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ClosetFragment parentfrag=((ClosetFragment)ShkafButtons.this.getParentFragment());
+                    parentfrag.swapverhnyalist();
                 }
             });
             return view;
@@ -65,6 +113,34 @@ public class ClosetFragment extends Fragment{
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             return inflater.inflate(R.layout.fragment_niz_list, container, false);
+        }
+    }
+    public static class ObuvList extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_obuv_list, container, false);
+        }
+    }
+    public static class SlitnoeList extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_slitnoe_list, container, false);
+        }
+    }
+    public static class VerhList extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_verh_list, container, false);
+        }
+    }
+    public static class VerhnyaList extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_verhnya_list, container, false);
         }
     }
 }
